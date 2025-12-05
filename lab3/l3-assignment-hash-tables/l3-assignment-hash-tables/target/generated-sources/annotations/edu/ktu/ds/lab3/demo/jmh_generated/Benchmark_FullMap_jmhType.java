d00fa3ab1a92a5d9733709201284ccbac2a96c20
Mert Çayır <93105900+Mekopa@users.noreply.github.com>
2025-12-05 11:16:39 +0200

    chore: Add target/ to .gitignore and remove build artifacts
    - Added target/ (Maven build folder) to .gitignore
    - Removed all compiled .class files from git tracking
    - These files are regenerated on each build

--- lab3/l3-assignment-hash-tables/l3-assignment-hash-tables/target/generated-sources/annotations/edu/ktu/ds/lab3/demo/jmh_generated/Benchmark_FullMap_jmhType.java
+++
@@ -1,4 +0,0 @@
-package edu.ktu.ds.lab3.demo.jmh_generated;
-public class Benchmark_FullMap_jmhType extends Benchmark_FullMap_jmhType_B3 {
-}
-
--- lab3/l3-assignment-hash-tables/l3-assignment-hash-tables/target/generated-sources/annotations/edu/ktu/ds/lab3/demo/jmh_generated/Benchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package edu.ktu.ds.lab3.demo.jmh_generated;
-public class Benchmark_jmhType extends Benchmark_jmhType_B3 {
-}
-
